57a6c0758d533b1a98c9d296740149881eb0291b
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-01-31 14:06:44 +0100

    php-snack-b1

--- snack1/index.php
+++ snack1/index.php
@@ -6,9 +6,40 @@ punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55 - 60
  -->
-<?php 
-$matches = []
-?>
+ <?php 
+ $matches = [
+     [
+         squadra1 => "Los Angeles Lakers",
+         punti1 => "80",
+         squadraGuest => "Golden State Warriors ",
+         puntiGuest => "50",
+     ],
+     [
+        squadra1 => "Cleveland Cavaliers",
+        punti1 => "60",
+        squadraGuest => "Los Angeles Clippers ",
+        puntiGuest => "30",
+     ],
+    [
+        squadra1 => "Dallas Mavericks",
+        punti1 => "40",
+        squadraGuest => "Denver Nuggets",
+        puntiGuest => "78",
+    ],
+    [
+        squadra1 => "Los Angeles Lakers",
+        punti1 => "80",
+        squadraGuest => "Golden State Warriors ",
+        puntiGuest => "50",
+    ],
+    [
+        squadra1 => "Portland",
+        punti1 => "47",
+        squadraGuest => "Chicago Bulls ",
+        puntiGuest => "85",
+    ]
+ ]
+ ?>
 
 
 
@@ -20,6 +51,16 @@ $matches = []
     <title>Document</title>
 </head>
 <body>
-    
+<h1>Squadre Snack 1</h1>
+<p><?php 
+    for ($i= 0; $i < count($matches); $i++) { 
+        # code... Olimpia Milano - Cantù | 55 - 60 lo schema che devo seguire quindi:
+        #metto la i dentro le parentesi quadre per indicare che deve prendere ogni elemento della mia array
+        echo $matches[$i] ["squadra1"]. " - " .$matches[$i] ["squadraGuest"].  " / " .$matches[$i]["punti1"]. " - " .$matches[$i]["puntiGuest"];
+
+    }
+
+?></p>
+      
 </body>
 </html>
